c4724d6ec623593b908e4abcd9066826b6dc9c33
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-03-24 16:47:13 +0530

    Setup CORS headers

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 
 import feedRoutes from './routes/feed';
@@ -7,6 +7,16 @@ const app: Express = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded  <form></form>
 app.use(bodyParser.json()); // application/json
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use('/feed', feedRoutes);
 
